c6a339b77c22909944d88d2ad022e3ce8ed62400
Alexander Raish <alexander.raish@gmail.com>
2026-02-22 18:01:05 +0700

    Add CLI argument parsing and REPL functionality.
    Integrate `clap` for command-line argument parsing, enabling script execution or interactive mode (REPL). Add logging with `env_logger`, a basic `run` function, and initial script example. Configure `rustfmt` for consistent formatting. Update dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,49 @@
+use std::{
+    io::Write,
+    path::{Path, PathBuf},
+};
+
+use clap::Parser;
+use log::{debug, error, info};
+
 mod scanner;
 
+#[derive(Debug, Parser)]
+struct RloxArgs {
+    /// Path to the script file to execute
+    script: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::builder().filter_level(log::LevelFilter::Trace).init();
+
+    let args = RloxArgs::parse();
+    if let Some(script) = args.script {
+        info!("Executing script: {}", script);
+        run_file(PathBuf::from(&script).as_path());
+    } else {
+        info!("Run in interactive mode.");
+        repl();
+    }
+}
+
+fn run_file(path: &Path) {
+    match std::fs::read_to_string(path) {
+        Ok(contents) => run(contents),
+        Err(e) => error!("Failed to read file: {}", e),
+    }
+}
+
+fn repl() {
+    loop {
+        let mut input = String::new();
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        run(input);
+    }
+}
+
+fn run(input: String) {
+    debug!("Running: {}", input);
 }
